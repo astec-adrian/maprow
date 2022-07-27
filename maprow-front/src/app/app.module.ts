3b3ff1348fefce1ed6deda7db1f54311785414df
fxkfjla <kacper.skr11@gmail.com>
2022-07-27 14:46:45 +0200

    add http client module

--- maprow-front/src/app/app.module.ts
+++ maprow-front/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './component/person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
